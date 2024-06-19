3d855c9d6291d5a655551054b97117fd1f9baa36
talia76 <talie76@gmx.de>
2024-06-19 11:46:37 +0200

    Update HomePageTest.java
    New

--- src/test/java/com/loolu/tests/HomePageTest.java
+++ src/test/java/com/loolu/tests/HomePageTest.java
@@ -1,11 +1,18 @@
 package com.loolu.tests;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
+
 public class HomePageTest {
+    WebDriver driver;
+
     private String URL = "https://localhost:3000//";
+
+
 
 }
 
