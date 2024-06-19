14c337ba700f171dd26da5cb5c6856a2aa7b3ddd
talia76 <talie76@gmx.de>
2024-06-19 11:28:34 +0200

    Update3

---
+++ src/test/java/com/loolu/tests/HomePageTest.java
@@ -0,0 +1,23 @@
+package com.loolu.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+    @BeforeMethod
+    public void ensurePrecondition(){
+        if(!app.getHomePage().isHomeComponentPresent()){
+            app.getHomePage().clickOnHomeLink();
+        }
+
+    }
+
+    @Test
+    public void isHomeComponentPresentTest() {
+        // System.out.println("Home component is " + isHomeComponentPresent());
+        Assert.assertTrue(app.getHomePage().isHomeComponentPresent());
+    }
+
+}
+}
