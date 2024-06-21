9c1edd63dcba3014c7069e2f97653e38cc6460ee
Antonina76 <139695921+Antonina76@users.noreply.github.com>
2024-06-21 21:46:07 +0200

    update LoginTest
    Update

--- src/test/java/com/loolu/tests/LoginTest.java
+++ src/test/java/com/loolu/tests/LoginTest.java
@@ -1,8 +1,13 @@
 package com.loolu.tests;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest extends TestBase {
     @Test
@@ -12,11 +17,14 @@ public class LoginTest extends TestBase {
         type(By.name("email"), "a.p@ap.com");
         type(By.name("password"), "Qwerty007!");
         click(By.className("User_submit__KttEJ"));
-        clickOnTheGuest();
-        click(By.cssSelector("p:nth-child(1) "));
-        type(By.name("username"), "Toni");
-      //  Assert.assertTrue(isElementPresent(By.cssSelector("p:nth-child(4)")));
+     click(By.className("User_close__cXNYs"));
+        click(By.className("Header_username__S3AFn"));
+        click(By.cssSelector("p:nth-child(4) "));
+     //   type(By.name("username"), "Toni");
+    //  Assert.assertTrue(isElementPresent(By.cssSelector("p:nth-child(4)")));
 //enter username
     }
 
-}
+    }
+
+
